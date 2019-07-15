da93ffd03e1d53d4d469ba20ed47273e33a2769c
Linas Nikiperavicius <linas@linasdev.com>
2019-07-15 22:30:43 +0300

    Added Pwm events.

---
+++ common/include/prettyhome/events/pwm_event.hpp
@@ -0,0 +1,289 @@
+/*
+ * Copyright (c) 2019, Linas Nikiperavicius
+ *
+ * This file is part of the PrettyHome project.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Linas Nikiperavicius <linas@linasdev.com>, 2019
+ */
+
+#ifndef PRETTYHOME_PWM_EVENT_HPP
+#define PRETTYHOME_PWM_EVENT_HPP
+
+#include <prettyhome/events/event.hpp>
+
+namespace prettyhome
+{
+	namespace events
+	{
+		enum class PwmEvent : uint16_t
+		{
+			BASE = 0x02 << 8,
+
+			REQUEST_STATUS,
+			STATUS_READY,
+
+			ENABLE,
+			DISABLE,
+
+			REQUEST_CHANNEL,
+			CHANNEL_READY,
+
+			UPDATE_CHANNEL,
+
+			REQUEST_RGBW_CHANNEL,
+			RGBW_CHANNEL_READY,
+
+			UPDATE_RGBW_CHANNEL,
+
+			UPDATE_SUCCESS,
+			ERROR
+		};
+
+		enum class PwmStatus : uint8_t
+		{
+			ENABLED,
+			DISABLED
+		}
+
+		enum class PwmError : uint8_t
+		{
+			CHANNEL_OUT_OF_BOUNDS,
+			VALUE_OUT_OF_BOUNDS
+		}
+
+		struct PwmRgbwValue
+		{
+			uint16_t red;
+			uint16_t green;
+			uint16_t blue;
+		};
+
+		class PwmRequestStatusEvent : public Event
+		{
+		public:
+			PwmRequestStatusEvent(EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::REQUEST_STATUS), callback)
+			{
+			}
+
+			PwmRequestStatusEvent(uint16_t causeId, EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::REQUEST_STATUS), causeId, callback)
+			{
+			}
+		};
+
+		class PwmStatusReadyEvent : public Event
+		{
+		public:
+
+			PwmStatusReadyEvent(uint8_t status, EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::STATUS_READY), callback), status(status)
+			{
+			}
+
+			PwmStatusReadyEvent(uint8_t status, uint16_t causeId, EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::STATUS_READY), causeId, callback), status(status)
+			{
+			}
+
+			uint8_t
+			getStatus() const;
+		private:
+			uint8_t status;
+		};
+
+		class PwmEnableEvent : public Event
+		{
+		public:
+			PwmEnableEvent(EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::ENABLE), callback)
+			{
+			}
+
+			PwmEnableEvent(uint16_t causeId, EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::ENABLE), causeId, callback)
+			{
+			}
+		};
+
+		class PwmDisableEvent : public Event
+		{
+		public:
+			PwmDisableEvent(EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::DISABLE), callback)
+			{
+			}
+
+			PwmDisableEvent(uint16_t causeId, EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::DISABLE), causeId, callback)
+			{
+			}
+		};
+
+		class PwmRequestChannelEvent : public Event
+		{
+		public:
+			PwmRequestChannelEvent(uint16_t channel, EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::REQUEST_CHANNEL), callback), channel(channel)
+			{
+			}
+
+			PwmRequestChannelEvent(uint16_t channel, uint16_t causeId, EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::REQUEST_CHANNEL), causeId, callback), channel(channel)
+			{
+			}
+
+			uint16_t
+			getChannel() const;
+		private:
+			uint16_t channel;
+		};
+
+		class PwmChannelReadyEvent : public Event
+		{
+		public:
+			PwmChannelReadyEvent(uint16_t channel, uint16_t value, EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::CHANNEL_READY), callback), channel(channel), value(value)
+			{
+			}
+
+			PwmChannelReadyEvent(uint16_t channel, uint16_t value, uint16_t causeId, EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::CHANNEL_READY), causeId, callback), channel(channel), value(value)
+			{
+			}
+
+			uint16_t
+			getChannel() const;
+
+			uint16_t
+			getValue() const;
+		private:
+			uint16_t channel;
+			uint16_t value;
+		};
+
+		class PwmUpdateChannelEvent : public Event
+		{
+		public:
+			PwmUpdateChannelEvent(uint16_t channel, uint16_t value, EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::UPDATE_CHANNEL), callback), channel(channel), value(value)
+			{
+			}
+
+			PwmUpdateChannelEvent(uint16_t channel, uint16_t value, uint16_t causeId, EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::UPDATE_CHANNEL), causeId, callback), channel(channel), value(value)
+			{
+			}
+
+			uint16_t
+			getChannel() const;
+
+			uint16_t
+			getValue() const;
+		private:
+			uint16_t channel;
+			uint16_t value;
+		};
+
+		class PwmRequestRgbwChannelEvent : public Event
+		{
+		public:
+			PwmRequestChannelEvent(uint16_t channel, EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::REQUEST_RGBW_CHANNEL), callback), channel(channel)
+			{
+			}
+
+			PwmRequestChannelEvent(uint16_t channel, uint16_t causeId, EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::REQUEST_RGBW_CHANNEL), causeId, callback), channel(channel)
+			{
+			}
+
+			uint16_t
+			getChannel() const;
+		private:
+			uint16_t channel;
+		};
+
+		class PwmRgbwChannelReadyEvent : public Event
+		{
+		public:
+			PwmChannelReadyEvent(uint16_t channel, PwmRgbwValue value, EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::RGBW_CHANNEL_READY), callback), channel(channel), value(value)
+			{
+			}
+
+			PwmChannelReadyEvent(uint16_t channel, PwmRgbwValue value, uint16_t causeId, EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::RGBW_CHANNEL_READY), causeId, callback), channel(channel), value(value)
+			{
+			}
+
+			uint16_t
+			getChannel() const;
+
+			PwmRgbwValue
+			getValue() const;
+		private:
+			uint16_t channel;
+			PwmRgbwValue value;
+		};
+
+		class PwmRgbwUpdateChannelEvent : public Event
+		{
+		public:
+			PwmUpdateChannelEvent(uint16_t channel, PwmRgbwValue value, EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::UPDATE_RGBW_CHANNEL), callback), channel(channel), value(value)
+			{
+			}
+
+			PwmUpdateChannelEvent(uint16_t channel, PwmRgbwValue value, uint16_t causeId, EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::UPDATE_RGBW_CHANNEL), causeId, callback), channel(channel), value(value)
+			{
+			}
+
+			uint16_t
+			getChannel() const;
+
+			PwmRgbwValue
+			getValue() const;
+		private:
+			uint16_t channel;
+			PwmRgbwValue value;
+		};
+
+		class PwmUpdateSuccessEvent : public Event
+		{
+		public:
+			PwmUpdateSuccessEvent(EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::UPDATE_SUCCESS), callback)
+			{
+			}
+
+			PwmUpdateSuccessEvent(uint16_t causeId, EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::UPDATE_SUCCESS), causeId, callback)
+			{
+			}
+		};
+
+		class PwmErrorEvent : public Event
+		{
+		public:
+			PwmErrorEvent(PwmError error, EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::ERROR), callback), error(error)
+			{
+			}
+
+			PwmErrorEvent(PwmError error, uint16_t causeId, EventCallback callback = nullptr)
+				: Event(static_cast< uint16_t > (PwmEvent::ERROR), causeId, callback), error(error)
+			{
+			}
+
+			PwmError
+			getError() const;
+		private:
+			PwmError error;
+		};
+	}
+}
+#endif  // PRETTYHOME_PWM_EVENT_HPP
---
+++ common/src/prettyhome/events/pwm_event.cpp
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2019, Linas Nikiperavicius
+ *
+ * This file is part of the PrettyHome project.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Linas Nikiperavicius <linas@linasdev.com>, 2019
+ */
+
+#include <prettyhome/events/pwm_event.hpp>
+
+namespace prettyhome
+{
+	namespace events
+	{
+		uint8_t
+		PwmStatusReadyEvent::getStatus() const
+		{
+			return status;
+		}
+
+
+		uint16_t
+		PwmRequestChannelEvent::getChannel() const
+		{
+			return channel;
+		}
+
+
+		uint16_t
+		PwmChannelReadyEvent::getChannel() const
+		{
+			return channel;
+		}
+
+		uint16_t
+		PwmChannelReadyEvent::getValue() const
+		{
+			return value;
+		}
+
+
+		uint16_t
+		PwmUpdateChannelEvent::getChannel() const
+		{
+			return channel;
+		}
+
+		uint16_t
+		PwmUpdateChannelEvent::getValue() const
+		{
+			return value;
+		}
+
+
+		uint16_t
+		PwmRequestRgbwChannelEvent::getChannel() const
+		{
+			return channel;
+		}
+
+
+		uint16_t
+		PwmRgbwChannelReadyEvent::getChannel() const
+		{
+			return channel;
+		}
+
+		PwmRgbwValue
+		PwmRgbwChannelReadyEvent::getValue() const
+		{
+			return value;
+		}
+
+
+		uint16_t
+		PwmUpdateRgbwChannelEvent::getChannel() const
+		{
+			return channel;
+		}
+
+		PwmRgbwValue
+		PwmUpdateRgbwChannelEvent::getValue() const
+		{
+			return value;
+		}
+
+
+		PwmError
+		PwmErrorEvent::getError() const
+		{
+			return error;
+		}
+	}
+}
