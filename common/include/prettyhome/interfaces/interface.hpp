1378200819d5c5da17a9d0e7cc7cc02056e3e064
Linas Nikiperavicius <linas@linasdev.com>
2019-07-18 15:52:55 +0300

    Added interface interface xd.

---
+++ common/include/prettyhome/interfaces/interface.hpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2019, Linas Nikiperavicius
+ *
+ * This file is part of the PrettyHome project.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Linas Nikiperavicius <linas@linasdev.com>, 2019
+ */
+
+#ifndef PRETTYHOME_INTERFACE_HPP
+#define PRETTYHOME_INTERFACE_HPP
+
+#include <cstdint>
+#include <queue>
+
+#include <modm/processing/protothread.hpp>
+#include <modm/processing/resumable.hpp>
+#include <prettyhome/events/event.hpp>
+
+namespace prettyhome
+{
+	namespace interfaces
+	{
+		static constexpr uint8_t BASE_INTERFACE_ID = 0x00;
+
+		class Interface : public modm::pt::Protothread
+		{
+		public:
+			Interface() = default;
+
+			virtual uint8_t
+			getInterfaceTypeId() const = 0;
+		protected:
+			std::queue< std::shared_ptr< events::Event > > eventQueue;
+			std::unordered_map< events::EventSelector, std::vector< events::EventCallback > > eventSubscriptions;
+
+			virtual bool
+			run() = 0;
+
+			void
+			reportEvent(std::shared_ptr< events::Event > event);
+		private:
+			Interface(const Module&) = delete;
+
+			Interface&
+			operator=(const Interface&) = delete;
+
+			void
+			subscribeEvent(events::EventSelector selector, events::EventCallback subscription);
+		};
+	}
+}
+
+#endif  // PRETTYHOME_INTERFACE_HPP
---
+++ common/src/prettyhome/interfaces/interface.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2019, Linas Nikiperavicius
+ *
+ * This file is part of the PrettyHome project.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Linas Nikiperavicius <linas@linasdev.com>, 2019
+ */
+
+#include <prettyhome/modules/module.hpp>
+#include <prettyhome/system.hpp>
+#include <prettyhome/log/logger.hpp>
+
+namespace prettyhome
+{
+	namespace interfaces
+	{
+		void
+		Interface::subscribeEvent(events::EventSelector selector, events::EventCallback subscription)
+		{
+			eventSubscriptions[selector].push_back(subscription);
+		}
+
+		void
+		Interface::reportEvent(std::shared_ptr< events::Event > event)
+		{
+			eventQueue.push(event);
+		}
+	}
+}
+
+#endif  // PRETTYHOME_INTERFACE_HPP
